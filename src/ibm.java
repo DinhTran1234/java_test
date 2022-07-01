2bb74980caf5b7da883d233b8318e1dfd0031396
Tran Dinh <trandinh@MacBook-Pro-cua-Tran.local>
2022-07-01 15:31:30 +0700

    file ibm

--- src/ibm.java
+++ src/ibm.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ibm {
+public class  ibm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double weight, height, bmi;
